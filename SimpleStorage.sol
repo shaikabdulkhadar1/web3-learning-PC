a6801d92738986841c6d4d918719a178b89d52e5
SAK-Remix <sak-remix@gmail.com>
2024-01-01 22:42:25 -0500

    first-commit

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -15,4 +15,8 @@ contract SimpleStorage {
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
+
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
 }
